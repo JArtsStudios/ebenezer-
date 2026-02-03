d568309ada7edd00cb15db8a8bacf2bf4d6f9a8f
JArtsStudios <168565464+JArtsStudios@users.noreply.github.com>
2026-02-03 15:32:29 -0600

    Update redirection logic in splash.js

--- js/splash.js
+++ js/splash.js
@@ -6,12 +6,13 @@ document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
         if (hasVisitedCuenta) {
             // Si ya se ha visitado cuenta.html, redirigir a inicio.html
-            window.location.href = "../inicio/";
+            window.location.href = "inicio/";
         } else {
             // Si no se ha visitado cuenta.html, redirigir a cuenta.html
-            window.location.href = "../inicio/";
+            window.location.href = "inicio/";
             // Marcar que ya se ha visitado cuenta.html
             localStorage.setItem("hasVisitedCuenta", "true");
         }
     }, 3000); // 2000 milisegundos = 2 segundos
+
 });
